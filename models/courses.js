2b7d055a0dc305aaa7457fe59f8dc1d03933240a
MariahRojas <51241518+MariahRojas@users.noreply.github.com>
2019-09-03 13:58:25 -0400

    Updates
    - Downloaded project files
    -  created model files in models folder
    - defined a model
    - definbed associations

---
+++ models/courses.js
@@ -0,0 +1,40 @@
+'use strict';
+const Sequelize = require('sequelize');
+
+module.exports = (sequelize) => {
+  class Course extends Sequelize.Model {}
+  Course.init({
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+    },
+    estimatedTime: {
+      type: Sequelize.STRING,
+      allowNull: false, //nullable
+    },
+    materialsNeeded: {
+      type: Sequelize.STRING,
+      allowNull: false, //nullable
+    },
+  }, { sequelize });
+
+  Course.associate = (models) => {
+    // TODO Add associations.
+    Course.belongsTo(models.User);
+  };
+
+  return Course;
+};
